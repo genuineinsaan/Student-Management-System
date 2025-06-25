9df4206685c7880bd856157ba166b541940354c7
Shashank Srivastava <145271166+genuineinsaan@users.noreply.github.com>
2025-06-25 12:22:11 +0530

    main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
+from tkinter import Tk
+from New_Login_Page import LoginApp  
+
+def main():
+    root = Tk()
+    app = LoginApp(root)  
+    root.mainloop()
+
+if __name__ == "__main__":
+    main()
 
